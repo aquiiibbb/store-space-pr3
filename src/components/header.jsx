5efb7ccf157b10e8d4d55f5dee5d13fbab9d3841
Nitin Zain <nitinshaukia@gmail.com>
2026-02-18 19:56:13 +0530

    Removed node_modules properly

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./header.css";
 import image from '../Assest/store-logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Header() {
   // Function to scroll to top when nav link is clicked
@@ -23,13 +25,13 @@ export default function Header() {
           </div>
 
           {/* Mobile Toggle */}
-          <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
-            aria-controls="navbarNav" 
-            aria-expanded="false" 
+            aria-controls="navbarNav"
+            aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
@@ -38,33 +40,33 @@ export default function Header() {
           {/* Collapsible Content */}
           <div className="collapse navbar-collapse" id="navbarNav">
             <div className="navbar-nav">
-              <NavLink 
-                to="/" 
-                end 
+              <NavLink
+                to="/"
+                end
                 className="nav-link cinderella-nav-link"
                 onClick={handleNavClick}
               >
                 HOME
               </NavLink>
-              <NavLink 
-                to="/rent" 
+              <NavLink
+                to="/rent"
                 className="nav-link cinderella-nav-link"
                 onClick={handleNavClick}
               >
                 RENT STORAGE
               </NavLink>
-              <NavLink 
-                to="/contact" 
+              <NavLink
+                to="/contact"
                 className="nav-link cinderella-nav-link"
                 onClick={handleNavClick}
               >
                 CONTACT US
               </NavLink>
             </div>
-            
+
             <div className="cinderella-cta-wrapper ms-auto">
               <button className="cinderella-reserve-button">
-               <a style={{textDecoration:"none"}} href="/form"> RESERVE NOW →</a>
+                <a style={{ textDecoration: "none" }} href="/form"> RESERVE NOW →</a>
               </button>
             </div>
           </div>
